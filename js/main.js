a7d440cecb13658264d559857a248866a0eb64a3
Davide Benazzi <benazzi.davide@gmail.com>
2020-10-21 15:09:49 +0200

    Ossatura e referenze

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('js ok!');
+// REFERENZE
+var nome = document.getElementById('nome');
+var chilometri = document.getElementById('chilometri');
+var eta = document.getElementById('eta');
+var genera = document.getElementById('genera');
+var annulla = document.getElementById('annulla');
+var biglietto = document.getElementById('biglietto');
+var dettagli = document.getElementById('dettagli');
+var offerta = document.getElementById('offerta');
+var carrozza = document.getElementById('carrozza');
+var codice = document.getElementById('codice');
+var costo = document.getElementById('costo');
